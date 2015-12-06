e8b2e25c92ecc9e5b1d4bf62648f92fe91e05c1a
sethvincent <sethvincent@gmail.com>
2015-12-06 14:42:57 -0500

    improve input

--- ui/save-world.js
+++ ui/save-world.js
@@ -3,15 +3,19 @@ var popup = require('popupjs')
 module.exports = function (payload) {
   var save = popup({
     id: 'popup',
-    content: '<h2>save your world</h2><input id="world-name" value="world" type="text" class="text-input" autofocus>',
+    content: '<h2>save your world</h2><input id="world-name" value="world.json" type="text" class="text-input" autofocus>',
     buttons: [
       {
         text: 'save world',
         className: 'save-world',
         fn: function () {
           var input = document.getElementById('world-name')
+          var world = input.value
+          if (world.substr(world.length - 5) !== '.json') {
+            world = world + '.json'
+          }
           var el = document.getElementById('button-save-download')
-          el.setAttribute('download', input.value + '.json')
+          el.setAttribute('download', world)
           el.setAttribute('href', 'data:application/text,' + payload)
           el.click()
           save.remove()
